33f5b0f31b97143a4b9c2769e2ba85718494d6cb
eros <nistico@teraria.com>
2022-02-11 01:35:13 +0100

    csafawsccxa

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,10 +3,6 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("test");
-		System.out.println("test");
-		System.out.println("test");
-		System.out.println("test");
 	}
 
 }
